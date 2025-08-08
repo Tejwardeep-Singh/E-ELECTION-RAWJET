5386f6034e91c2b1ab8e15899d65bafc484f440d
Tejwardeep Singh <tejwardeep@gmail.com>
2025-08-08 09:57:24 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -38,9 +38,7 @@ app.use("/api/election",electionRoutes);
 app.use("/api/results",resultsRouter);
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+
 // Start server
 
 app.listen(PORT, () => {
